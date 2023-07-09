6d4726ffdd0be5ed854061e28d04a76e655241a4
Axel Gastón Roitstein <axelroitstein@gmail.com>
2023-07-09 19:38:58 -0300

    corrijo desafío de clase 24

--- clase24/desafio-clase-24.js
+++ clase24/desafio-clase-24.js
@@ -1,5 +1,15 @@
 const ONE_MS = 1000
 
+/* IIFE
+for (var i = 1; i <= 10; i++) {
+  const timer = (x) => {
+    setTimeout(() => {
+      console.log(x)
+    }, x * ONE_MS)
+  }
+  timer(i)
+} */
+
 for (let i = 1; i <= 10; i++) {
   setTimeout(() => {
     console.log(i)
